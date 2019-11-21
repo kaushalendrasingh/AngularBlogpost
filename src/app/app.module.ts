3fab4e4f7c5cd1781c5f1e0d3ec21ffc867d6206
kaushalendra singh <40608033+kaushalendrasingh@users.noreply.github.com>
2019-11-21 19:58:44 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule, Title} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { BlogpostModule } from './blogpost/blogpost.module';
+import { CmspageModule } from './cmspage/cmspage.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+import { AdminModule } from './admin/admin.module';
+import { AuthModule } from './auth/auth.module';
+
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { httpInterceptorProviders } from './http-interceptors/index';
+import { AuthInterceptor } from './http-interceptors/auth-interceptor';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BlogpostModule,
+    CmspageModule,
+    AdminModule,
+    AuthModule,
+    AppRoutingModule
+  ],
+  providers: [Title, 
+    httpInterceptorProviders,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
